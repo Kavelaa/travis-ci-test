28da392db7bc501c9d192618a5d38c8ff221df71
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-05 16:55:10 +0000

    Deploy Kavelaa/travis-ci-test to github.com/Kavelaa/travis-ci-test.git:gh-pages

---
+++ precache-manifest.627bd745946396511be79d501c0dcef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4f4914111903f466482a87c322272f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c17e090d7ef1d7433b24",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "f37849c704a9785d5469",
+    "url": "/static/js/2.751bb574.chunk.js"
+  },
+  {
+    "revision": "c17e090d7ef1d7433b24",
+    "url": "/static/js/main.9f1aa413.chunk.js"
+  },
+  {
+    "revision": "66ec99c66fc63932591b",
+    "url": "/static/js/runtime-main.0cc5ba3c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
